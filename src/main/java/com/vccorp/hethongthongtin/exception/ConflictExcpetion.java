9d62ad1ad6c953ab0067806f8ad06ba6c7badb34
ToThaiDuong <tothaiduong507@gmail.com>
2024-05-22 15:47:58 +0700

    added exception

---
+++ src/main/java/com/vccorp/hethongthongtin/exception/ConflictExcpetion.java
@@ -0,0 +1,7 @@
+package com.vccorp.hethongthongtin.exception;
+
+public class ExistedException extends RuntimeException{
+    public ExistedException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/vccorp/hethongthongtin/exception/NotFoundException.java
@@ -0,0 +1,2 @@
+package com.vccorp.hethongthongtin.exception;public class NotFoundException {
+}
